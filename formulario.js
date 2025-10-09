192b367b1c1150c5e27712711f329196349d6b1b
VictorSS7 <victor.ssandu17@gmail.com>
2025-10-09 16:27:07 +0200

    Pequeños cambios

---
+++ formulario.js
@@ -0,0 +1,9 @@
+
+
+comprobarCampos = () => {
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let email = document.getElementById("email").value;
+    let reserva = document.getElementById("reserva").value;
+}
+
