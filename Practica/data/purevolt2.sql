04d6a8e1a1fb0bb492412415040e6df57eef7f14
VictorSS7 <victor.ssandu17@gmail.com>
2025-12-07 20:13:59 +0100

    sql

---
+++ Practica/data/purevolt2.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2025 at 08:13 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `purevolt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `concesionarios`
+--
+
+CREATE TABLE `concesionarios` (
+  `id_concesionario` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `nombre` varchar(100) NOT NULL,
+  `ciudad` varchar(100) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `telefono_contacto` char(9) NOT NULL,
+  `latitud` float NOT NULL,
+  `longitud` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id_reserva` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `id_usuario` int(11) NOT NULL,
+  `id_vehiculo` int(11) NOT NULL,
+  `fecha_inicio` datetime NOT NULL,
+  `fecha_fin` datetime NOT NULL,
+  `estado` enum('activa','finalizada','cancelada') NOT NULL DEFAULT 'activa',
+  `kilometros_recorridos` int(11) DEFAULT NULL,
+  `incidencias_reportadas` text DEFAULT NULL,
+  `valoracion` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`valoracion`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `rol` enum('empleado','admin') NOT NULL,
+  `telefono` char(9) DEFAULT NULL,
+  `id_concesionario` int(11) DEFAULT NULL,
+  `foto_perfil` varchar(255) NOT NULL DEFAULT 'noUser.png',
+  `preferencias_accesibilidad` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`preferencias_accesibilidad`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `activo`, `nombre`, `correo`, `contrasena`, `rol`, `telefono`, `id_concesionario`, `foto_perfil`, `preferencias_accesibilidad`) VALUES
+(1, 1, 'Administrador General', 'admin@purevolt.es', '$2b$10$8ApUW/TFMo.b6a/VCf5k7eEF1HiIHIT6g52hTuC2Ey/r9ekxWhh2O', 'admin', '600000000', NULL, 'noUser.png', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehiculos`
+--
+
+CREATE TABLE `vehiculos` (
+  `id_vehiculo` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `matricula` char(8) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `anyo_matriculacion` year(4) NOT NULL,
+  `numero_plazas` tinyint(3) UNSIGNED NOT NULL CHECK (`numero_plazas` > 0),
+  `autonomia_km` int(10) UNSIGNED DEFAULT NULL,
+  `autonomia_actual` int(10) UNSIGNED NOT NULL,
+  `color` varchar(30) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `estado` enum('disponible','reservado','mantenimiento') NOT NULL DEFAULT 'disponible',
+  `id_concesionario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `concesionarios`
+--
+ALTER TABLE `concesionarios`
+  ADD PRIMARY KEY (`id_concesionario`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indexes for table `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `fk_reserva_usuario` (`id_usuario`),
+  ADD KEY `fk_reserva_vehiculo` (`id_vehiculo`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `correo` (`correo`),
+  ADD KEY `fk_usuario_concesionario` (`id_concesionario`);
+
+--
+-- Indexes for table `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD PRIMARY KEY (`id_vehiculo`),
+  ADD UNIQUE KEY `matricula` (`matricula`),
+  ADD KEY `fk_vehiculo_concesionario` (`id_concesionario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `concesionarios`
+--
+ALTER TABLE `concesionarios`
+  MODIFY `id_concesionario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  MODIFY `id_vehiculo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `fk_reserva_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_reserva_vehiculo` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculos` (`id_vehiculo`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_usuario_concesionario` FOREIGN KEY (`id_concesionario`) REFERENCES `concesionarios` (`id_concesionario`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD CONSTRAINT `fk_vehiculo_concesionario` FOREIGN KEY (`id_concesionario`) REFERENCES `concesionarios` (`id_concesionario`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
